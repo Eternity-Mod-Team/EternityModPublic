7787e78bb4cf934870a2fa7eea9febe43a1760c5
Vaema <vaema.arcadia@gmail.com>
2025-01-03 04:51:19 -0800

    Updated build and description text files.

---
+++ Items/Weapons/Melee/CryogenicBlade.cs
@@ -0,0 +1,33 @@
+﻿//using EternityMod.Projectiles.Melee;
+//using Terraria;
+//using Terraria.ID;
+//using Terraria.ModLoader;
+
+//namespace EternityMod.Items.Weapons.Melee
+//{
+//    public class CryogenicBlade : ModItem
+//    {
+//        public new string LocalizationCategory => "Items.Weapons.Ranged";
+
+//        public override void SetDefaults()
+//        {
+//            Item.width = 28;
+//            Item.height = 30;
+//            Item.DamageType = DamageClass.Melee;
+//            Item.damage = 40;
+//            Item.crit = 15;
+//            Item.knockBack = 2f;
+//            Item.useTime = 4;
+//            Item.useAnimation = 4;
+//            Item.useAmmo = ItemID.Gel;
+//            Item.autoReuse = true;
+//            Item.channel = true;
+//            Item.useStyle = ItemUseStyleID.Swing;
+//            Item.UseSound = SoundID.Item1;
+//            Item.rare = ItemRarityID.Green;
+//            Item.value = Item.sellPrice(0, 2, 50);
+//            Item.shoot = ModContent.ProjectileType<CryogenicSwing>();
+//            Item.shootSpeed = 4.5f;
+//        }
+//    }
+//}
---
+++ Projectiles/Melee/CryogenicSwing.cs
@@ -0,0 +1,18 @@
+﻿/*using Terraria.ModLoader;
+
+namespace EternityMod.Projectiles.Melee
+{
+    public class CryogenicSwing : ModProjectile
+    {
+        private enum State
+        {
+            Swing,
+            Spin
+        }
+
+        public override void SetDefaults()
+        {
+            
+        }
+    }
+}*/
