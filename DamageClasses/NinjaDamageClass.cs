be24d7f43a702a72bdd1a7877c42e29f530032af
Vaema <vaema.arcadia@gmail.com>
2025-01-14 15:06:02 -0800

    Added the Ninja damage class.

---
+++ DamageClasses/NinjaDamageClass.cs
@@ -0,0 +1,22 @@
+﻿using Terraria.ModLoader;
+
+namespace EternityMod.DamageClasses
+{
+    public class NinjaDamageClass : DamageClass
+    {
+        internal static NinjaDamageClass Instance;
+
+        public override void Load() => Instance = this;
+        public override void Unload() => Instance = null;
+
+        public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
+        {
+            if (damageClass == Throwing || damageClass == Generic)
+                return StatInheritanceData.Full;
+
+            return StatInheritanceData.None;
+        }
+
+        public override bool GetEffectInheritance(DamageClass damageClass) => damageClass == Throwing;
+    }
+}
